fbfa25213d14603847f2712e46d9d9e302d2f08f
2001740 <2001740@NC22A-03>
2021-10-21 20:51:50 +0200

    Toevoeging Pakket Class

--- PakketZending/Program.cs
+++ PakketZending/Program.cs
@@ -10,6 +10,33 @@ namespace PakketZending
     {
         static void Main(string[] args)
         {
+            string keuze;
+            double Inv_Lengte,Inv_Breedte,Inv_Hoogte,Inv_Gewicht;
+            Console.WriteLine("Welkom bij de post, indien je wilt stoppen type exit.");
+            keuze = "";
+
+            while (keuze != "exit" )
+            {                
+                Console.WriteLine("Gelieve de volgende gegevens van het paketje even in te geven");
+                Console.Write("Lengte: ");
+                Inv_Lengte = Convert.ToDouble(Console.ReadLine().Replace(".",","));
+                Console.Write("Breedte: ");
+                Inv_Breedte = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+                Console.Write("Hoogte: ");
+                Inv_Hoogte = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+                Console.Write("Gewicht: ");
+                Inv_Gewicht = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+                Pakket p = new Pakket(Inv_Lengte,Inv_Breedte,Inv_Hoogte,Inv_Gewicht);
+                
+
+
+                Console.Write("Exit?: ");
+                keuze = Console.ReadLine();
+            }
+            
+            
+
         }
     }
 }
