1b9e3180221a4cee14129300e7468c672dbe0b2b
Marcel <marcel.leeuwen@gmail.com>
2021-10-28 20:53:21 +0200

    Created hulpprogramma

---
+++ PakketZending/Hulpprogramma.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PakketZending
+{
+    public class Hulpprogramma
+    {
+
+        //Returns Double value na replace . -> , en convert to string
+        public static double CheckInputDouble(string input)
+        {
+            double result = 0d;
+            try
+            {
+                input = input.Replace("m", "");
+                input = input.Replace(".", ",");
+                result = Convert.ToDouble(input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Geen gelding input!!!!");
+                Console.Write("Geef nieuw waarde in: ");
+                result = CheckInputDouble(Console.ReadLine());
+
+            }
+            return result;
+        }
+    }
+}
--- PakketZending/Program.cs
+++ PakketZending/Program.cs
@@ -17,20 +17,20 @@ namespace PakketZending
 
             //Loop tot Exit of Y (alle vormen toegestaan)
             while (!keuze.ToLower().Equals("exit") && !keuze.ToLower().Equals("y"))
-            {                
+            {
                 Console.WriteLine("Gelieve de volgende gegevens van het paketje even in te geven");
-                Console.WriteLine("Geldige Afmetingen :" );
+                Console.WriteLine("Geldige Afmetingen :");
                 Console.WriteLine("Lengte korter dan 0,5m / Breedte smaller dan 0,4m / Hoogte kleiner dan 0,6m ");
                 Console.Write("Lengte: ");
-                Inv_Lengte = CheckInputDouble(Console.ReadLine());
+                Inv_Lengte = Hulpprogramma.CheckInputDouble(Console.ReadLine());
                 Console.Write("Breedte: ");
-                Inv_Breedte = CheckInputDouble(Console.ReadLine());
+                Inv_Breedte = Hulpprogramma.CheckInputDouble(Console.ReadLine());
                 Console.Write("Hoogte: ");
-                Inv_Hoogte = CheckInputDouble(Console.ReadLine());
+                Inv_Hoogte = Hulpprogramma.CheckInputDouble(Console.ReadLine());
                 Console.Write("Gewicht: ");
-                Inv_Gewicht = CheckInputDouble(Console.ReadLine());
+                Inv_Gewicht = Hulpprogramma.CheckInputDouble(Console.ReadLine());
 
-                Pakket p = new Pakket(Inv_Lengte,Inv_Breedte,Inv_Hoogte,Inv_Gewicht);
+                Pakket p = new Pakket(Inv_Lengte, Inv_Breedte, Inv_Hoogte, Inv_Gewicht);
                 Zending zend = new Zending();
 
                 Console.Write("Zend Postocde: ");
@@ -44,29 +44,6 @@ namespace PakketZending
                 Console.Write("Exit?: ");
                 keuze = Console.ReadLine();
             }
-            
-            
-
-        }
-
-        //Returns Double value na replace . -> , en convert to string
-        public static double CheckInputDouble(string input)
-        {
-            double result=0d;
-            try
-            {
-                input = input.Replace("m", "");
-                input = input.Replace(".", ",");
-                result = Convert.ToDouble(input);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Geen gelding input!!!!");
-                Console.Write("Geef nieuw waarde in: ");
-                result = CheckInputDouble(Console.ReadLine());
-
-            }
-            return result;
         }
     }
 }
